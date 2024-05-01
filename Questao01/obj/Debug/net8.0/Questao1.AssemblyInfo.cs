563e57ac6d98ce33d6dfe9e1c44fac15103fa71e
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-05-01 14:44:46 -0300

    Alteracao dos nomes das pastas

--- Questao01/obj/Debug/net8.0/Questao1.AssemblyInfo.cs
+++ Questao01/obj/Debug/net8.0/Questao1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a08c5c446c13e81d792e82db0a5192c769c8798")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
